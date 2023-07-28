0e19451242a9eea07f2467d322e78d81626a840c
Ch4kr4k <marakmuna9@gmail.com>
2023-07-28 14:41:09 +0530

    update

--- library.c
+++ library.c
@@ -1,26 +1,32 @@
-// ls
+// library
 #include <stdio.h>
 #include <dirent.h>
-  
+#include <string.h>
+
 extern int list_files()
 {
     struct dirent *list_of_files;
 	FILE *fptr = fopen("list.txt", "w");
     DIR *dr = opendir(".");
+
   
     if (dr == NULL)
     {
-        printf("Could not open current directory" );
+        printf("cannot open" );
         return 0;
     }
 
     while ((list_of_files = readdir(dr)) != NULL)
 	{
-            printf("%s\n", list_of_files -> d_name);
-			fprintf(fptr,"%s\n", list_of_files -> d_name);
-			
+		if (strstr(list_of_files -> d_name, ".c" ))
+			fprintf(fptr,"%s ", list_of_files -> d_name);
+		else
+			continue;	
 	}
 	fclose(fptr);
     closedir(dr);
     return 0;
 }
+
+	
+
--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 // main.c
+#include <stdio.h>
 #include "library.h"
-
+#define GCC "C:\Users\SI0896\Downloads\test\softwares\MinGW-master\MinGW\bin>gcc.exe"
 int main(void)
 {
+	char ch, ch1[100];
+	int i=0;
+	int count = 0;
+	
 	list_files();
 	
+	FILE *fptr = fopen("list.txt", "r");
+	
+	do{
+		ch = fgetc(fptr);
+		ch1[count] = ch;
+		count++;
+	}while(ch != EOF);
+
 	return 0;
 }
